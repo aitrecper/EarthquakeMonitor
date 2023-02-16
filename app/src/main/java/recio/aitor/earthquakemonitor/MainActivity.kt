159a1096bc6b9be4a1c987faa8cfe03f872d93cf
Aitor Reci Pérez <aitrecper@gmail.com>
2023-02-16 13:55:38 +0100

    Preparing viewmodel and retrofit

--- app/src/main/java/recio/aitor/earthquakemonitor/MainActivity.kt
+++ app/src/main/java/recio/aitor/earthquakemonitor/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import recio.aitor.earthquakemonitor.databinding.ActivityMainBinding
 
@@ -14,28 +16,31 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.eqRecycler.layoutManager = LinearLayoutManager(this)
-        val eqList = mutableListOf<Earthquake>()
-        eqList.add(Earthquake("1","Buenos Aires", 4.3, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("2","Lima", 2.9, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("3","Ciudad de México", 6.0, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("4","Bogotá", 1.8, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("5","Caracas", 3.5, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("6","Madrid   ", 0.6, 273846152L, -102.4756, 28.47365))
-        eqList.add(Earthquake("7","Acra", 5.1, 273846152L, -102.4756, 28.47365))
-
         val adapter = EqAdapter()
-
         binding.eqRecycler.adapter = adapter
-        adapter.submitList(eqList)
+
+        binding.eqRecycler.layoutManager = LinearLayoutManager(this)
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        viewModel.eqList.observe(this, Observer {
+            eqList ->
+            adapter.submitList(eqList)
+            handleEmptyView(eqList, binding)
+        })
 
         adapter.onItemClickListener = {
             Toast.makeText(this,it.place,Toast.LENGTH_SHORT).show()
         }
 
-        if (eqList.isEmpty()){
+    }
+
+    private fun handleEmptyView(
+        eqList: MutableList<Earthquake>,
+        binding: ActivityMainBinding
+    ) {
+        if (eqList.isEmpty()) {
             binding.eqEmptyView.visibility = View.VISIBLE
-        } else{
+        } else {
             binding.eqEmptyView.visibility = View.GONE
         }
     }
